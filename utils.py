bbcd883d53251b8f5e9c231d7551f07dc5be7109
timchaura <timchaura@gmail.com>
2022-04-19 01:13:02 +0300

    + hardcoded receipts

--- utils.py
+++ utils.py
@@ -7,7 +7,16 @@ from devices import Phone, TV, Notebook
 from receipt import Receipt
 
 
-receiptsdict = {}
+receiptsdict = {1: Receipt(1, Phone("Xiaomi", "Android", "MIUI is shit pls help"),
+                           "2022-02-24", "2022-02-26", "Antony", Receipt.list_of_statuses[2]),
+                2: Receipt(2, TV("Samsung", "27", "Screen is white"), "2022-03-05",
+                           "2022-03-11", "Andrey", Receipt.list_of_statuses[2]),
+                3: Receipt(3, Notebook("Asus", "Windows 11", "2012-03-02", "Doesn't start"),
+                           "2022-02-13", "2022-02-15", "Dmitry", Receipt.list_of_statuses[2]),
+                4: Receipt(4, TV("LG", 40, "Doesn't work"), "2021-04-25", "2021-04-30", "Vitally",
+                           Receipt.list_of_statuses[2]),
+                5: Receipt(5, Phone("Samsung", "Android", "Works slowly"),
+                           "2021-07-15", "2021-07-17", "Max", Receipt.list_of_statuses[2])}
 
 
 def create_repair_request():
